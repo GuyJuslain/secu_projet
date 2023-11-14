ddf1719b3d61087d1c62d5431e9cce90955c9166
Guyjuslain <guyjuslain@gmail.com>
2023-11-14 13:19:34 -0500

    Premiere ebauche de connexion entre un cleint et un serveur. Le serveur imprime jusle les messages recu du client

--- src/app_a.adb
+++ src/app_a.adb
@@ -1,6 +1,44 @@
 
-with Ada.Text_IO; use Ada.Text_IO; 
-procedure app_a is
+with Ada.Text_IO;             use Ada.Text_IO;
+with GNAT.Sockets;            use GNAT.Sockets;
+WITH Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+with Ada.Streams;   use Ada.Streams;
+use type Ada.Streams.Stream_Element_Count;
+
+procedure client is
+   Client  : Socket_Type;
+   Address : Sock_Addr_Type;
+   Channel : Stream_Access; 
+   Message : Unbounded_String := To_Unbounded_String("");
+
+   Send   : String := (1 => ASCII.CR, 2 => ASCII.LF, 3 => ASCII.CR, 4 => ASCII.LF);
+   Offset : Ada.Streams.Stream_Element_Count;
+   Data   : Ada.Streams.Stream_Element_Array (1 .. 256);
+   count : Integer := 0;
+
 begin
-   Put("Je vous salut depuis App_A");
-end app_a;
+   GNAT.Sockets.Initialize;
+   Create_Socket (Client);
+
+   -- Utilisez l'adresse IP et le port du serveur
+   Address.Addr := Inet_Addr("127.0.0.1");
+   Address.Port := 12321;
+
+   Connect_Socket (Client, Address);
+   Channel := Stream (Client);
+   Put_Line("Je me connecte : ");
+   Put_Line("");
+   delay 0.5;
+   Put("ENVOIE DE MESSAGES AU SERVEUR");
+   Put("-------------------------------------------------");
+   Put_Line("");
+   loop
+      exit when count = 5;
+      delay 0.5;
+      put("Saisisez votre message :  ");
+      Message := To_Unbounded_String(Get_Line);
+      String'Write (Channel, To_String(Message)  & Send);
+      count := count + 1;
+   end loop;
+end client;
--- src/secu_projet.adb
+++ src/secu_projet.adb
@@ -1,6 +1,36 @@
 
-with Ada.Text_IO; use Ada.Text_IO; 
-procedure secu_Projet is
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.IO_Exceptions; use Ada.IO_Exceptions;
+with GNAT.Sockets;      use GNAT.Sockets;
+with Ada.Streams;       use Ada.Streams;
+
+procedure serveur is
+
+   Receiver   : GNAT.Sockets.Socket_Type;
+   Connection : GNAT.Sockets.Socket_Type;
+   Client     : GNAT.Sockets.Sock_Addr_Type;
+   Channel    : GNAT.Sockets.Stream_Access;
+   Offset : Ada.Streams.Stream_Element_Count;
+   Data   : Ada.Streams.Stream_Element_Array (1 .. 256);
+
+
+
 begin
-   Put("Salut tout le monde depuis APP_Middle !!!");
-end secu_Projet;
+   Create_Socket (Socket => Receiver);
+   Set_Socket_Option(Socket => Receiver, Level  => Socket_Level, Option => (Name => Reuse_Address, Enabled => True));
+   Bind_Socket(Socket => Receiver, Address => (Family => Family_Inet, Addr => Inet_Addr ("127.0.0.1"), Port => 12321));
+   Listen_Socket (Socket => Receiver);
+
+   loop
+      Accept_Socket(Server => Receiver, Socket  => Connection, Address => Client);
+      Put_Line("Client connected from " & GNAT.Sockets.Image (Client));
+      Channel := Stream (Connection);
+      loop
+         Ada.Streams.Read (Channel.All, Data, Offset);
+         exit when Offset = 0;
+         for I in 1 .. Offset loop
+            Ada.Text_IO.Put (Character'Val(Data(I)));
+         end loop;
+      end loop;
+   end loop;
+end serveur;
