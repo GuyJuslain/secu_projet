2b9e983a2aa09b6ca0079150b3617cc4e2f49cd5
Guyjuslain <guyjuslain@gmail.com>
2023-11-17 18:25:01 -0500

    version améliorée et fonctionnelle de la premiere écbauche

--- src/app_a.adb
+++ src/app_a.adb
@@ -1,10 +1,11 @@
 
 with Ada.Text_IO;             use Ada.Text_IO;
 with GNAT.Sockets;            use GNAT.Sockets;
-WITH Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+WITH Ada.Strings.Unbounded;   use Ada.Strings.Unbounded;
 
+-- Streams pour les flux
 with Ada.Streams;   use Ada.Streams;
-use type Ada.Streams.Stream_Element_Count;
+use type Ada.Streams.Stream_Element_Count;   
 
 procedure client is
    Client  : Socket_Type;
@@ -12,9 +13,9 @@ procedure client is
    Channel : Stream_Access; 
    Message : Unbounded_String := To_Unbounded_String("");
 
-   Send   : String := (1 => ASCII.CR, 2 => ASCII.LF, 3 => ASCII.CR, 4 => ASCII.LF);
-   Offset : Ada.Streams.Stream_Element_Count;
-   Data   : Ada.Streams.Stream_Element_Array (1 .. 256);
+ --  Send   : String := (1 => ASCII.CR, 2 => ASCII.LF, 3 => ASCII.CR, 4 => ASCII.LF);
+ --  Offset : Ada.Streams.Stream_Element_Count;
+ --  Data   : Ada.Streams.Stream_Element_Array (1 .. 256);
    count : Integer := 0;
 
 begin
@@ -37,8 +38,9 @@ begin
       exit when count = 5;
       delay 0.5;
       put("Saisisez votre message :  ");
-      Message := To_Unbounded_String(Get_Line);
-      String'Write (Channel, To_String(Message)  & Send);
+      Message := To_Unbounded_String(Get_Line) & ASCII.LF;  -- On ecrit le message et on lui ajoute un caractère de fin de ligne
+      String'Write (Channel, To_String(Message));   
       count := count + 1;
-   end loop;
+   end loop;   
+ --  String'Write (Channel, ASCII.NUL'Image);   
 end client;
--- src/secu_projet.adb
+++ src/secu_projet.adb
@@ -3,17 +3,18 @@ with Ada.Text_IO;       use Ada.Text_IO;
 with Ada.IO_Exceptions; use Ada.IO_Exceptions;
 with GNAT.Sockets;      use GNAT.Sockets;
 with Ada.Streams;       use Ada.Streams;
+WITH Ada.Strings.Unbounded;   use Ada.Strings.Unbounded;
 
 procedure serveur is
 
-   Receiver   : GNAT.Sockets.Socket_Type;
-   Connection : GNAT.Sockets.Socket_Type;
-   Client     : GNAT.Sockets.Sock_Addr_Type;
-   Channel    : GNAT.Sockets.Stream_Access;
-   Offset : Ada.Streams.Stream_Element_Count;
-   Data   : Ada.Streams.Stream_Element_Array (1 .. 256);
-
-
+   Receiver       : GNAT.Sockets.Socket_Type;
+   Connection     : GNAT.Sockets.Socket_Type;
+   Client         : GNAT.Sockets.Sock_Addr_Type;
+   Channel        : GNAT.Sockets.Stream_Access;
+   Offset         : Ada.Streams.Stream_Element_Count;      --off
+   Data           : Ada.Streams.Stream_Element_Array (1 .. 1);
+   receive_message   : Unbounded_String;
+   char_lu : Character := ' ';
 
 begin
    Create_Socket (Socket => Receiver);
@@ -21,16 +22,38 @@ begin
    Bind_Socket(Socket => Receiver, Address => (Family => Family_Inet, Addr => Inet_Addr ("127.0.0.1"), Port => 12321));
    Listen_Socket (Socket => Receiver);
 
-   loop
+   -- Boucle pour l'écoute des connexions
+
+   loop        
       Accept_Socket(Server => Receiver, Socket  => Connection, Address => Client);
       Put_Line("Client connected from " & GNAT.Sockets.Image (Client));
       Channel := Stream (Connection);
-      loop
-         Ada.Streams.Read (Channel.All, Data, Offset);
-         exit when Offset = 0;
-         for I in 1 .. Offset loop
-            Ada.Text_IO.Put (Character'Val(Data(I)));
+
+   -- On lit les messages qui arrivent. On lit La serie de message que le client connecté envoie
+
+      loop     
+         receive_message := To_Unbounded_String("");
+
+   -- On boucle pour lire un message entier (Lecture caractere par caractere)
+
+         loop     
+            Ada.Streams.Read(Channel.All, Data, Offset);
+            char_lu := Character'Val(Data(1));
+            exit when char_lu = ASCII.LF;    -- On lit caractere par carctere jusqu'à ce qu'on ait un caractère fin de ligne (LF)
+            Append(receive_message, char_lu);
+ --           receive_message := receive_message & char_lu'Image;
          end loop;
+         Put(To_String(receive_message));
+         Put_Line("");
+         exit when Offset = 0;   
       end loop;
+      
    end loop;
 end serveur;
+
+--https://stackoverflow.com/questions/21577579/how-to-properly-read-and-write-on-sockets-using-ada
+
+--https://en.wikibooks.org/wiki/Ada_Programming/Libraries/GNAT.Sockets
+
+
+--https://www.adaic.org/resources/add_content/standards/05rm/html/RM-J-5.html
